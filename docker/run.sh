7a6326e760919549816c42c04d4c65aca3c71cc4
Rene Kochen <rene.kochen@merapar.com>
2017-05-19 10:33:58 +0200

    Stateful set introduced

--- docker/run.sh
+++ docker/run.sh
@@ -3,8 +3,6 @@
 sed -i -e "s/{{listen_address}}/$POD_IP/g" /cassandra/conf/cassandra.yaml
 sed -i -e "s/{{rpc_address}}/$POD_IP/g" /cassandra/conf/cassandra.yaml
 
-JVM_OPTS="-Dcassandra.ring_delay_ms=30000 -Dmx4jaddress=0.0.0.0 -Dmx4jport=8081"
-
 cd /cassandra/bin
 
 exec ./cassandra -f
