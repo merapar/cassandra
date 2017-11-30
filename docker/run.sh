adc374c0ae4a9940617037b3fee091a862b977bf
Unknown <rene.kochen@merapar.com>
2017-11-30 22:21:15 +0100

    disable mmap

--- docker/run.sh
+++ docker/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Give kube-dns some time, otherwise cassandra-0.cassandra cannot be resolved when starting the seed node
+sleep 5
+
 sed -i -e "s/{{listen_address}}/$POD_IP/g" /cassandra/conf/cassandra.yaml
 sed -i -e "s/{{rpc_address}}/$POD_IP/g" /cassandra/conf/cassandra.yaml
 
